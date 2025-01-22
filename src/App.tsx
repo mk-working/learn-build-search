4c2133677de6cb96e4c86e0b23eb7caeadbead52
mk-working <mk25.working@gmail.com>
2025-01-22 13:53:13 +0700

    File Strucutre for Zustand:

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useState } from 'react'
 import './App.css'
-import SearchBar from './SearchBar'
-import SearchResult from './SearchResult'
-import { fetchUsers } from './api/user';
+
 
 function App() {
-  const[results, setResults] = useState([]);
-
-  const handleSearch = async (value: string) => {
-    const data = await fetchUsers(value);
-    setResults(data)
-  }
 
   return (
     <>
-      <SearchBar onSearch={handleSearch}/>
-      <SearchResult results={results}/>
     </>
   )
 }
